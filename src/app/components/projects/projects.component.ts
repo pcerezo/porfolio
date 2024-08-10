a3c957ca1f3328403d27e183e7f501244b420c7b
pcerezo <pcerezo70@gmail.com>
2024-08-10 20:46:23 +0200

    Mejorada la presentación de los proyectos. Añadidas imágenes de portada en cada uno junto con un botón con el que conducirá a la información detallada del proyecto.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -9,12 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
   projects = [
-    { title: 'Title 1', body: 'Texto de prueba ' },
-    { title: 'Title 2', body: 'Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.' },
-    { title: 'Title 3', body: 'Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.' },
+    { title: 'Web de medición de la contaminación lumínica', 
+      body: 'Trabajo de fin de grado presentado en la universidad de Granada donde se mide la contaminación lumínica',
+      portada: 'contaminacion_luminica/portada'
+    },
+    { title: 'Chacobache', 
+      body: 'Promoción de un conjunto musical dedicado a la producción e interpretación musical muy variada especializada en el área de cine, anime, series y videojuegos',
+      portada: 'chacobache/portada'
+    },
+   /* { title: 'Title 3', body: 'Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.' },
     { title: 'Title 4', body: 'Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.' },
     { title: 'Title 5', body: 'Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.' },
-    { title: 'Title 6', body: 'Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.' }
+    { title: 'Title 6', body: 'Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.' }*/
   ];
 
   constructor() { }
--- src/app/components/tools/tools.component.ts
+++ src/app/components/tools/tools.component.ts
@@ -11,10 +11,23 @@ export class ToolsComponent implements OnInit{
   tools = [
     { nombre: 'Angular', icon: 'angular' },
     { nombre: 'Spring Boot', icon: 'springboot' },
-    { nombre: 'PHP', icon: 'php' },
+    { nombre: 'hibernate', icon: 'hibernate'},
     { nombre: 'Java', icon: 'java' },
     { nombre: 'Ruby', icon: 'ruby' },
-    { nombre: 'Bootstrap', icon: 'bootstrap' }
+    { nombre: 'C#', icon: 'CSharp'},
+    { nombre: 'C', icon: 'C'},
+    { nombre: 'C++', icon: 'c++'},
+    { nombre: 'Bootstrap', icon: 'bootstrap' },
+    { nombre: 'HTML', icon: 'html'},
+    { nombre: 'css', icon: 'css'},
+    { nombre: 'Javascript', icon: 'javascript'},
+    { nombre: 'git', icon: 'git'},
+    { nombre: 'github', icon: 'github'},
+    { nombre: 'PHP', icon: 'php' },
+    { nombre: 'NodeJS', icon: 'nodejs'},
+    { nombre: 'Thymeleaf', icon: 'thymeleaf'},
+    { nombre: 'Oracle SQL', icon: 'oracleSQL'},
+    { nombre: 'Mongo DB', icon: 'mongoDB'}
   ];
 
   constructor() { }
