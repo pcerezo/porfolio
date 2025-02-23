b5d17c0d8de19fe320f14ca6e1f536c0e983c5ce
pcerezo <pcerezo70@gmail.com>
2025-02-23 11:53:49 +0100

    Mejorada la presentación del frontend y añadido acceso a componente de Contact. Falta añadir vinculación con el backend para lanzar el envío de correo.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ProjectDetailComponent } from './components/project-detail/project-detail.component';
 import { DatosResolver } from './components/project-detail/project-detail-resolver';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
     {path: "", component: InicioComponent},
@@ -9,7 +10,8 @@ export const routes: Routes = [
     {path: "proyecto/:id", component: ProjectDetailComponent, 
         resolve: {
             project: DatosResolver
-        }}
+        }},
+    {path: "contacto", component: ContactComponent}
 
     //{path: "**", redirectTo: "inicio"}
 ];
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  imageExists: boolean = false;
+  imageUrl: string = '/assets/profile.png';
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.checkImageExists(this.imageUrl);
+  }
+
+  checkImageExists(url: string): void {
+    this.http.head(url, { observe: 'response' }).subscribe(
+      response => {
+        if (response.status === 200) {
+          this.imageExists = true;
+        }
+      },
+      error => {
+        this.imageExists = false;
+      }
+    );
+  }
 }
